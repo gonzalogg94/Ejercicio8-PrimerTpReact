93c6fe6c3c0a25baa0109d100f1ca1207113d123
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-09-29 21:15:47 -0300

    termine el ejercicio 8

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+
 const Formulario = () => {
+ const [nombre, setNombre] = useState("");
+ const [apellido, setApellido] = useState("");
+ const [dni, setDni] = useState("");
+
+ const handleSubmit=(e)=>{
+  e.preventDefault();
+  if(nombre===""||apellido===""||dni==="")
+  alert("faltan completar algunos datos")
+  else{
+    alert("los datos fueron cargados")
+  }
+ }
+
     return (
-        <Form className='mt-5'>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form  className='mt-5' onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="inputNombre"onChange={(e)=>setNombre(e.target.value)}
+          value={nombre}>
           <Form.Label>Nombre</Form.Label>
           <Form.Control type="text" placeholder="Ingrese su nombre" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3" controlId="inputApellido"onChange={(e)=>setApellido(e.target.value)}
+          value={apellido}>
           <Form.Label>Apellido</Form.Label>
           <Form.Control type="text" placeholder="Ingrese su apellido" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3" controlId="inputDni"onChange={(e)=>setDni(e.target.value)}
+          value={dni}>
           <Form.Label>Dni</Form.Label>
           <Form.Control type="number" placeholder="Ingrese su D.N.I" />
         </Form.Group>
-        <Button  variant="primary" type="submit">
+        <Button  variant="warning" type="submit">
           Enviar
         </Button>
       </Form>
